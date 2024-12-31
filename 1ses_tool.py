9fc42581f58ce737d59ccdc89da70d0caa40e42a
Yu Heechan <75111815+sbrm374@users.noreply.github.com>
2024-12-31 16:53:25 +0900

    Update 1ses_tool.py

--- 1ses_tool.py
+++ 1ses_tool.py
@@ -2,6 +2,13 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
+from matplotlib import rcParams
+
+# 폰트 설정 (한글 및 한자 깨짐 방지)
+rcParams['font.family'] = 'AppleGothic'  # MacOS의 경우
+# rcParams['font.family'] = 'Malgun Gothic'  # Windows의 경우
+# rcParams['font.family'] = 'NanumGothic'  # Linux의 경우
+rcParams['axes.unicode_minus'] = False  # 음수 기호 깨짐 방지
 
 # タイトル
 st.title("SES事業継続率管理ツール")
